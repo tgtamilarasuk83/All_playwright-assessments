2859e02c7808c2c6179d88d913090a0ae4062d79
Tamilarasu143 <tamilarasu143@gmail.com>
2026-07-06 07:50:59 +0530

    reort.js added

--- Automation_Execercie_Cucumber_Project/src/test/helper/report.js
+++ Automation_Execercie_Cucumber_Project/src/test/helper/report.js
@@ -1,20 +1,20 @@
-import * as report from "multiple-cucumber-html-reporter";
+const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "./reports",
-  reportPath: "./reports",
-  pageTitle: "Automation Exercise Report",
-  reportName: "Automation Exercise Execution Report",
-  displayDuration: true,
-  metadata: {
-    browser: {
-      name: "Chrome",
-      version: "Latest"
-    },
-    device: "Local Test Machine",
-    platform: {
-      name: "Windows",
-      version: "11"
+    jsonDir: "./reports",
+    reportPath: "./reports",
+    pageTitle: "Automation Exercise Report",
+    reportName: "Automation Exercise Execution Report",
+    displayDuration: true,
+    metadata: {
+        browser: {
+            name: "Chrome",
+            version: "Latest"
+        },
+        device: "Local Test Machine",
+        platform: {
+            name: "Windows",
+            version: "11"
+        }
     }
-  }
 });
